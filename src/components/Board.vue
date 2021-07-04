ab591b19fa70ab1313c0915dab478acd99296e14
dahye <laveberry7@gmail.com>
2021-07-04 19:42:15 +0900

    상세보기

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,25 +2,27 @@
   <div>
     <!-- <b-table striped hover :items="items"></b-table> -->
     <!-- fields 속성 추가 -->
-    <b-table striped hover :items="items" :fields="fields"></b-table>
-
+    <b-table striped hover :items="items" :fields="fields" @row-clicked="rowClick"></b-table>
+    <b-button @click="writeContent">글쓰기</b-button>
   </div>
 </template>
 
 <script>
 import data from '@/data'
-
-    //sort() : 인자로 함수를 받아서 오름차순(a-b), 내림차순(b-a)
-    let items = data.Content.sort((a,b) => {return b.content_id - a.content_id})
-    /**
-     map() : 배열내 모든함수 각각에 대해 새로운배열 반환. 1번째인자(배열각각의 아이템), 리턴값(객체), 새로운배열 속성추가
-    filter() : 주어진 판별함수를 통과하는 요소를모아, 새로운 배열로 만들어 반환(ex. a가 속한요소만 가져오기)
-     */
-    //1. map으로 user_name속성 추가해주기
-    //2. user_id의 속성은 한개뿐이니 0번쨰 인덱스로 가져옴
-    items = items.map(contentItem => {return{...contentItem, user_name: data.User.filter(userItem => userItem.user_id === contentItem.user_id)[0].name}})
+  
     export default {
         data() {
+            
+            //sort() : 인자로 함수를 받아서 오름차순(a-b), 내림차순(b-a)
+            let items = data.Content.sort((a,b) => {return b.content_id - a.content_id})
+            /**
+             map() : 배열내 모든함수 각각에 대해 새로운배열 반환. 1번째인자(배열각각의 아이템), 리턴값(객체), 새로운배열 속성추가
+            filter() : 주어진 판별함수를 통과하는 요소를모아, 새로운 배열로 만들어 반환(ex. a가 속한요소만 가져오기)
+            */
+            //1. map으로 user_name속성 추가해주기
+            //2. user_id의 속성은 한개뿐이니 0번쨰 인덱스로 가져옴
+            items = items.map(contentItem => {return{...contentItem, user_name: data.User.filter(userItem => userItem.user_id === contentItem.user_id)[0].name}})
+
             return {
                 //데이터 불러오기
                 // items: data.Content,
@@ -54,6 +56,19 @@ import data from '@/data'
             //   { age: 38, first_name: 'Jami', last_name: 'Carney' }
             // ]
             
+            }
+        },
+
+        methods: {
+            rowClick(item, index, e) {
+                this.$router.push({
+                    path: `/board/free/detail/${item.content_id}`
+                })
+            },
+            writeContent() {
+                this.$router.push({
+                    path: '/board/free/create'
+                })
             }
         }
     }
